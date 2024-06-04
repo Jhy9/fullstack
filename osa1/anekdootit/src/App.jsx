fbc9585d18f698324edbb1efd4532ce07ab98ec9
Jhy9 <juusohyyrylainen@hotmail.com>
2024-06-04 19:21:24 +0300

    osa1 loppu

--- osa1/anekdootit/src/App.jsx
+++ osa1/anekdootit/src/App.jsx
@@ -6,6 +6,8 @@ const Button = ({handleClick,text}) => (
   </button>
 )
 
+const Header = ({title}) => <h2>{title}</h2>
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -17,19 +19,35 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
     'The only way to go fast, is to go well.'
   ]
-
   const [selected, setSelected] = useState(0)
-  const [] = useState(0)
+  const [votes, voteFor] = useState(new Array(anecdotes.length).fill(0))
+  const [mostVotes, changeBest] = useState(0)
+
   const randomAnecdote=()=>{
     const size = anecdotes.length
     setSelected(Math.floor(Math.random()*size))
   }
 
+  const vote =(index)=>{  
+    const handler= ()=>{
+      const copy = [...votes]
+      copy[index] += 1
+      if (copy[index] > copy[mostVotes]) changeBest(index)
+      voteFor(copy)
+    }
+    return handler
+  }
+
   return (
     <div>
-      {anecdotes[selected]}
-
-      <p><Button handleClick={randomAnecdote} text="next anecdote" /></p>
+      <Header title = "Anecdote of the day"/>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <p><Button handleClick={vote(selected)} text="vote" />
+      <Button handleClick={randomAnecdote} text="next anecdote" /></p>
+      <Header title = "Anecdote with most votes"/>
+      <p>{anecdotes[mostVotes]}</p>
+      <p>has {votes[mostVotes]} votes</p>
     </div>
   )
 }
